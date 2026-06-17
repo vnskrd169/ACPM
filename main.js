ccea0364ef687900ba5c9435331b5bc9334e8289
vnskrd169 <benczar.raboy@gmail.com>
2026-06-18 01:18:38 +0800

    fixing firebase

--- main.js
+++ main.js
@@ -4,7 +4,6 @@
 
 // ── Firebase Config (v8) ──────────────────────────────────
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
